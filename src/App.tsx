0571fce34ef281490401406dce1d90c80cf1f3c0
Tyler Russell <tyler.russell1@aol.com>
2023-05-30 14:41:03 -0500

    Basic skeleton.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/react.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <header>
+          <div className='card'>
+            <h2>Dallas by Metro</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula varius nisi, sit amet fermentum
+              metus finibus non. Proin vel lectus a enim dignissim ultricies. Vestibulum convallis, lorem ac tristique
+              ultrices, ante metus luctus est, eu dictum erat tortor vitae purus.</p>
+              <button type='submit'>Learn More</button>
+          </div>
+          
+          <img src={logo} alt='Dallas skyline.'></img>
+        </header>
+
+        <section id='explore_downtown'>
+          <img src={logo} alt='Downtown Dallas.'></img>
+          <div className='card'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorem voluptatum doloremque cumque totam iste in repudiandae, accusamus possimus magni? Consequuntur repellat, commodi similique ducimus maiores neque beatae dignissimos a.</p>
+            <button type='submit'>Explore Downtown</button>
+          </div>
+        </section>
+
+        <section id='popular_destinations'>
+          <h2>Popular Destinations</h2>
+          <div className='destination-container'>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+          </div>
+        </section>
+        
+        <section id='planner'>
+          <h2>Create your own journey...</h2>
+          <p>With our carefully curated list you will find easily to get to and fun new locations.</p>
+          <form>
+
+          </form>
+          <div className='map'></div>
+        </section>
+        
+        <section id='contact_us'>
+          <h2>Contact</h2>
+          <form action="#" method="POST">
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div>
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" name="message" required></textarea>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+      </main>
+
+      <footer>
+        Connect with us on:
+      </footer>
     </>
   )
 }
