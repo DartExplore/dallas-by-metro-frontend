ea0b4db9b273a5cdac0249ce416fe79afa800bbf
Tyler Russell <tyler.russell1@aol.com>
2023-05-30 15:54:22 -0500

    Basic styling and layout.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <main>
-        <header>
+        <header className='basic-grid'>
           <div className='card'>
             <h2>Dallas by Metro</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula varius nisi, sit amet fermentum
@@ -18,8 +18,9 @@ function App() {
           <img src={logo} alt='Dallas skyline.'></img>
         </header>
 
-        <section id='explore_downtown'>
+        <section id='explore_downtown' className='basic-grid'>
           <img src={logo} alt='Downtown Dallas.'></img>
+
           <div className='card'>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorem voluptatum doloremque cumque totam iste in repudiandae, accusamus possimus magni? Consequuntur repellat, commodi similique ducimus maiores neque beatae dignissimos a.</p>
             <button type='submit'>Explore Downtown</button>
@@ -32,20 +33,46 @@ function App() {
             <div className='destination-item'></div>
             <div className='destination-item'></div>
             <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
+            <div className='destination-item'></div>
           </div>
         </section>
         
         <section id='planner'>
-          <h2>Create your own journey...</h2>
-          <p>With our carefully curated list you will find easily to get to and fun new locations.</p>
-          <form>
+          <div className='card'>
+            <h2>Create your own journey...</h2>
+            <p>With our carefully curated list you will find easily to get to and fun new locations.</p>
+          </div>
+          
+          <div className='basic-grid'>
+            <form action="#" method="POST">
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" required></textarea>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
 
-          </form>
-          <div className='map'></div>
+            <div className='map'></div>
+          </div>
+          
         </section>
         
         <section id='contact_us'>
           <h2>Contact</h2>
+          
           <form action="#" method="POST">
             <div>
               <label htmlFor="name">Name:</label>
@@ -65,7 +92,7 @@ function App() {
       </main>
 
       <footer>
-        Connect with us on:
+        <h2>Connect with us on:</h2>
       </footer>
     </>
   )
