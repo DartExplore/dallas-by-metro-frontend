431f591a489554f512f917925587fd37bcf7db0b
Tyler Russell <tyler.russell1@aol.com>
2023-06-06 11:47:35 -0500

    Removed contact form.

--- src/App.tsx
+++ src/App.tsx
@@ -75,26 +75,6 @@ function App() {
           </div>
           
         </section>
-        
-        <section id='contact_us'>
-          <h2>Contact</h2>
-          
-          <form action="#" method="POST">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" required />
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required />
-            </div>
-            <div>
-              <label htmlFor="message">Message:</label>
-              <textarea id="message" name="message" required></textarea>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
       </main>
 
       <footer>
