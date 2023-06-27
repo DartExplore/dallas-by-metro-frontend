d628503b2a13b7273a541b01c3f9439ad6d43d6a
Garrett Pennell <garrettpennell@gmail.com>
2023-06-26 22:08:41 -0500

    Use Prettier
    + set up prettier in pre-commit and GitHub Actions
    + add .eslintcache to .gitignore
    
    Resolves #71

--- src/components/PointOfInterestCard/PointOfInterestCard.tsx
+++ src/components/PointOfInterestCard/PointOfInterestCard.tsx
@@ -1,19 +1,23 @@
-import './PointOfInterestCard.scss';
-import PointOfInterest from '../interfaces/PointOfInterest';
+import "./PointOfInterestCard.scss";
+import PointOfInterest from "../interfaces/PointOfInterest";
 
 interface PointOfInterestCardProps {
-    pointOfInterest: PointOfInterest,
-    id: number
+  pointOfInterest: PointOfInterest;
+  id: number;
 }
 
-function PointOfInterestCard({pointOfInterest, id}: PointOfInterestCardProps) {
-    return (<>
-        <div className={'poi-container poi-border-' + id % 2}>
-            <h3>{pointOfInterest.name}</h3>
-            <p>Walk time: {pointOfInterest.walkingDistance}</p>
-        </div>
-
-    </>);
+function PointOfInterestCard({
+  pointOfInterest,
+  id,
+}: PointOfInterestCardProps) {
+  return (
+    <>
+      <div className={"poi-container poi-border-" + (id % 2)}>
+        <h3>{pointOfInterest.name}</h3>
+        <p>Walk time: {pointOfInterest.walkingDistance}</p>
+      </div>
+    </>
+  );
 }
 
 export default PointOfInterestCard;
