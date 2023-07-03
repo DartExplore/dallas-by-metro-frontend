7981943368a7f01f890be560ede7fe68492740b6
Garrett Pennell <garrettpennell@gmail.com>
2023-07-02 21:15:15 -0500

    Initial commit of separate Planner route
    Resolves #21

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Planner from "./components/Planner/Planner";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/planner" element={<Planner />} />
+        bod
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
