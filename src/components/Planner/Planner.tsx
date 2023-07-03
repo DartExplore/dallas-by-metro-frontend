66da226e0cf49c2232674494ed9e88da08ca3d60
Garrett Pennell <garrettpennell@gmail.com>
2023-07-02 21:15:15 -0500

    Initial commit of separate Planner route
    Resolves #21

---
+++ src/components/Planner/Planner.tsx
@@ -0,0 +1,15 @@
+import InputForm from "../InputForm/InputForm.tsx";
+import "./Planner.css";
+
+const Planner = () => {
+  return (
+    <main className="planner">
+      <header>
+        <h1>Find cool places</h1>
+      </header>
+      <InputForm />
+    </main>
+  );
+};
+
+export default Planner;
